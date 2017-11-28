4143408a268512e61db89176659f6cb539cdaa36
nschnitzer <33706405+nschnitzer@users.noreply.github.com>
2017-11-28 13:10:04 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+//*************************************************************
+// Nathan Schnitzer
+// HelloWorld.java
+// 11/28/17
+// This will print out Hello World for the Jboss task
+//*************************************************************
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+
+	}
+
+}
